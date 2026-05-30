d9b444cd7a015993147861b294082af46f0ff66f
stylobow <stylobow@outlook.fr>
2026-05-30 16:44:28 +0200

    Collision removed with CTF Block

--- src/main/java/fr/stylobow/iyc/block/custom/CtfRedBlock.java
+++ src/main/java/fr/stylobow/iyc/block/custom/CtfRedBlock.java
@@ -1,18 +1,13 @@
 package fr.stylobow.iyc.block.custom;
 
-import net.minecraft.ChatFormatting;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.BlockGetter;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.TransparentBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.shapes.CollisionContext;
-import net.minecraft.world.phys.shapes.EntityCollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
-import net.minecraft.world.scores.Team;
 
 public class CtfRedBlock extends TransparentBlock {
 
@@ -21,26 +16,27 @@ public class CtfRedBlock extends TransparentBlock {
     }
 
     @Override
-    public VoxelShape getCollisionShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
-        if (context instanceof EntityCollisionContext entityContext) {
-            if (entityContext.getEntity() instanceof Player player) {
-                Team team = player.getTeam();
-                if (team != null && team.getColor() == ChatFormatting.RED) {
-                    return Shapes.empty();
-                }
-            }
-        }
+    public VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
         return Shapes.block();
     }
 
     @Override
-    protected boolean skipRendering(BlockState state, BlockState adjacentBlockState, Direction direction) {
-        return adjacentBlockState.is(this) || super.skipRendering(state, adjacentBlockState, direction);
+    public VoxelShape getCollisionShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
+        if (context == CollisionContext.empty()) {
+            return Shapes.block();
+        }
+
+        return Shapes.empty();
     }
 
     @Override
     protected VoxelShape getVisualShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
         return Shapes.empty();
+    }
+
+    @Override
+    protected boolean skipRendering(BlockState state, BlockState adjacentBlockState, Direction direction) {
+        return adjacentBlockState.is(this) || super.skipRendering(state, adjacentBlockState, direction);
     }
 
     @Override
